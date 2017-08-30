cee14649f93a11da5cd5f983e4ae3d0a3ba49ee8
Maksim Drahun <DRAHUN.MAXIM@standardbank.co.za>
2017-08-30 17:18:38 +0300

    template

--- src/app/settings/settings.module.ts
+++ src/app/settings/settings.module.ts
@@ -1,16 +1,17 @@
 import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 import {NgModule} from '@angular/core';
+import {TranslateModule} from '@ngx-translate/core';
 
 import {SettingsRoutingModule} from './settings-routing.module';
 
 import {CustomerInfoComponent} from './component/customer-info.component';
-
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    SettingsRoutingModule
+    SettingsRoutingModule,
+    TranslateModule.forChild()
   ],
   declarations: [
     CustomerInfoComponent
